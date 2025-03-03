ffc81e391943c820a20f4b263bd7ec1257857ea5
robert611 <cyrkiel86@interia.pl>
2025-03-03 23:31:00 +0100

    Add classroom migration

---
+++ database/migrations/2025_03_03_214628_create_classroom_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('classroom', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->string('description', 1024)->nullable();
+            $table->string('key', 12)->unique();
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classroom');
+    }
+};
